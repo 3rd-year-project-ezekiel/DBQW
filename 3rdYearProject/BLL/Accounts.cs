a1b9e16cdddd44dffdd623a364d2206ca095a2ce
ZeusOLP <ZeusOLP@OLYMPUS>
2019-04-29 18:26:13 +0200

    Adding Classes

---
+++ 3rdYearProject/BLL/Accounts.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using System.Data;
+using System.Collections;
+
+public class Accounts
+{
+    private string userName;
+    private string password;
+    private string connectionType;
+
+    public string UserName { get => userName; set => userName = value; }
+    public string Password { get => password; set => password = value; }
+    public string ConnectionType { get => connectionType; set => connectionType = value; }
+
+    public List<Accounts> SelectAllAccounts()
+    {
+        
+    }
+
+    public void InsertAccount()
+    {
+        
+    }
+}
