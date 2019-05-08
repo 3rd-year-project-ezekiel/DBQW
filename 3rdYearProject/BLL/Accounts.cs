0a0a63d9b6df79a07412583d5aa34c1e3c4330fe
LordFirehawk <39730717+LordFirehawk@users.noreply.github.com>
2019-05-08 23:22:42 +0200

    accounts check user method
    added the method to the account class that wil check if the user exists
    still needs to write the datahandler method

--- 3rdYearProject/BLL/Accounts.cs
+++ 3rdYearProject/BLL/Accounts.cs
@@ -9,23 +9,58 @@ using System.Collections;
 
 public class Accounts
 {
+    #region Fields
     private string userName;
     private string password;
     private string connectionType;
+    #endregion
 
-    // work
+    #region Constructors
+    public Accounts(string userName, string password, string connectionType)
+    {
+        this.userName = userName;
+        this.password = password;
+        this.connectionType = connectionType;
+    }
 
+    #endregion
+
+    #region properties
     public string UserName { get => userName; set => userName = value; }
     public string Password { get => password; set => password = value; }
     public string ConnectionType { get => connectionType; set => connectionType = value; }
 
-    public List<Accounts> SelectAllAccounts()
+    #endregion
+
+    #region override
+
+
+    #endregion
+
+    #region Methods
+    public bool SelectAllAccounts(Accounts UserInput)
     {
-        
+       
+        DBConnection handler = new DBConnection();
+        DataTable dataRaw = handler.AllUsers();
+
+        foreach(DataRow dataItem in dataRaw.Rows)
+        {
+
+            if ((dataItem["TableUserName"].ToString() == UserInput.UserName) && (dataItem["TablePassword"].ToString() == UserInput.Password))
+            {
+                return true;
+            }
+
+        }
+
+        return false;
     }
 
     public void InsertAccount()
     {
         
     }
+
+    #endregion
 }
