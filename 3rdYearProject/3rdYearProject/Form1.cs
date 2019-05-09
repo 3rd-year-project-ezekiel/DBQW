0eefdbd70336489b330349048ae6e6834ff887f0
LordFirehawk <39730717+LordFirehawk@users.noreply.github.com>
2019-05-09 09:20:24 +0200

    login pt1

--- 3rdYearProject/3rdYearProject/Form1.cs
+++ 3rdYearProject/3rdYearProject/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
 
+
 namespace _3rdYearProject
 {
     public partial class Form1 : Form
@@ -37,8 +38,9 @@ namespace _3rdYearProject
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Form2 form2 = new Form2();
-            form2.Show();
+            var Login = new Account
+            // Form2 form2 = new Form2();
+            // form2.Show();
         }
 
         private void TopNavPnl_MouseDown(object sender, MouseEventArgs e)
