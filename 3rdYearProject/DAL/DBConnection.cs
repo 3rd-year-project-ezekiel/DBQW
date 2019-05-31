56a293f03352f003467b527abb028a4c1c097099
Eziopuff <50193432+Eziopuff@users.noreply.github.com>
2019-05-31 21:44:28 +0200

    Update DBConnection.cs

--- 3rdYearProject/DAL/DBConnection.cs
+++ 3rdYearProject/DAL/DBConnection.cs
@@ -21,6 +21,80 @@ namespace DAL
         {
             this.connectionString = connectionStringParam;
         }
+        
+        public DataTable SelectQuery(SearchArray, TableArray, WhereArray, ConnectionArray = NULL)
+                {
+
+                    string query = "SELECT ";
+
+                    for(i = 0, i < SearchArray.length, i++) {
+                        query = query + SearchArray[i];
+                        if(i != SearchArray.length-1) {
+                            query = query + ", ";
+                        }
+                    } //SearchArray must contain the string variables of all things that must be searched
+
+                    query = query + " FROM ";
+
+                    for(i = 0, i < TableArray.length, i++) {
+                        query = query + TableArray[i];
+                        if(i != TableArray.length-1) {
+                            query = query + ", ";
+                        }
+                    } //TableArray must contain the tables that we are selecting from
+
+                    query = query + " WHERE "
+
+                    if(ConnectionArray != NULL) {
+                        for(i = 0, i < ConnectionArray.length, i++) {
+                        query = query + ConnectionArray[i] + " AND ";
+                    }//ConnectionArray contains the connection for the tablesArray
+                    }
+
+                    for(i = 0, i < WhereArray.length, i++) {
+                        query = query + WhereArray[i];
+                        if(i != WhereArray.length-1) {
+                            query = query + " AND ";
+                        }
+                    }// WhereArray contains the where clauses at the end of the select
+
+                    //Example of running it
+
+                    // Search Array : ENum, EName, ESurname, PRating, PPrice
+                    // Table Array : EMP, PROJ
+                    // Where Array : PRating = "Important", PPrice < 50000
+                    // Connection Array: EMP.ENum = PROJ.ENum
+
+                    // Output predicted: SELECT ENum, EName, ESurname, PRating, PPrice FROM EMP, PROJ WHERE EMP.ENum = PROJ. ENum AND PRating = "Important" AND PPrice < 50000
+
+                    //This is what I think... If you can tell me if it works
+
+                    for(i = 0, i < TableArray.length, i++)
+                    try
+                    {
+                        if (connection.State != ConnectionState.Open)
+                        {
+                            connection.Open();
+                        }
+
+                        command = new SqlCommand(query, connection);
+                        adapter = new SqlDataAdapter(command);
+
+                        table = new DataTable();
+                        adapter.Fill(table);
+                    }
+                    catch (Exception)
+                    {
+
+                        throw;
+                    }
+                    finally
+                    {
+                        connection.Close();
+                    }
+
+                    return table;
+                }
 
         public bool UserLogin()
         {
