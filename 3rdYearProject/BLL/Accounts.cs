e2acb5e23d55f806e068f91c502efb6bdc10dad4
ZeusOLP <ZeusOLP@OLYMPUS>
2019-06-01 00:33:04 +0200

    Dynamically add app.config

--- 3rdYearProject/BLL/Accounts.cs
+++ 3rdYearProject/BLL/Accounts.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 using DAL;
 using System.Data;
 using System.Collections;
+using System.IO;
+using System.Xml;
+using System.Configuration;
+using static System.Net.Mime.MediaTypeNames;
 
 public class Accounts
 {
@@ -62,7 +66,44 @@ public class Accounts
             connStrBldr.Password = this.Password;
         }
 
-        DBConnection handler = new DBConnection(connStrBldr.ToString());
+        string ApplicationPath = System.IO.Directory.GetCurrentDirectory();
+        string YourPath = Path.GetDirectoryName(ApplicationPath);
+        bool isNew = false;
+
+        string path = Path.GetDirectoryName(YourPath) + "\\App.config";
+        XmlDocument doc = new XmlDocument();
+        doc.Load(path);
+        XmlNodeList list = doc.DocumentElement.SelectNodes("connectionStrings/add[@name='default']");
+        XmlNode node;
+        isNew = list.Count == 0;
+        if (isNew)
+        {
+            node = doc.CreateNode(XmlNodeType.Element, "add", null);
+            XmlAttribute attribute = doc.CreateAttribute("name");
+            attribute.Value = "default";
+            node.Attributes.Append(attribute);
+
+            attribute = doc.CreateAttribute("connectionString");
+            attribute.Value = "";
+            node.Attributes.Append(attribute);
+
+            attribute = doc.CreateAttribute("providerName");
+            attribute.Value = "System.Data.SqlClient";
+            node.Attributes.Append(attribute);
+        }
+        else
+        {
+            node = list[0];
+        }
+        string conString = node.Attributes["connectionString"].Value;
+        node.Attributes["connectionString"].Value = connStrBldr.ConnectionString;
+        if (isNew)
+        {
+            doc.DocumentElement.SelectNodes("connectionStrings")[0].AppendChild(node);
+        }
+        doc.Save(path);
+
+        DBConnection handler = new DBConnection();
         validUser = handler.UserLogin();
 
         return validUser;
