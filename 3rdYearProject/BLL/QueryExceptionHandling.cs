a66bd651f83abe35f9ea9b3a01f8bc821210ec23
LordFirehawk <39730717+LordFirehawk@users.noreply.github.com>
2019-09-23 15:56:01 +0200

    tasks devided between JP and Jan

--- 3rdYearProject/BLL/QueryExceptionHandling.cs
+++ 3rdYearProject/BLL/QueryExceptionHandling.cs
@@ -8,25 +8,37 @@ namespace BLL
 {
     class QueryExceptionHandling
     {
+
+
+
+
+        // == JP == \\
         //Main form
         //tussen switch query en combo gives you an option if you want to switch 
-        //- no selected show the previous options
+        //- if no selected show the previous options
 
-        //Data type checking all 
-        /* tab - values, where, Set, Having, Procedures.
-         * table creation when column selected check if the data types are the same.
-         */
+        // Datatype checking 
+        // table creation when column selected check if the data types are the same.
 
-        //exception catcher but must not stop program.
 
+
+
+        // == Jan == \\
         //Color selection from menu strip.
+        // Data type checking all 
+        // tab - values, where, Set, Having, Procedures.  make sure that the right datatype is filled in
 
+
+        // == Both Reseach and Wikus == \\
+        //exception catcher but must not stop program. for everything
+        //  -- for access dinied when creating database
+        //  -- for when the querie is wrong somwhere
         //check if query will run or not run (using SQL built in function)
 
-        // main menu, on tabs. make sure that the right datatype is filled in
+
 
 
     }
 
-    
+
 }
