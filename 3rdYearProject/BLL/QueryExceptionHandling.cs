e4eadafc6b7da62682d5594333dc63481cf41539
Swazzi <49069865+Swazzi@users.noreply.github.com>
2019-09-23 15:48:13 +0200

    Update QueryExceptionHandling.cs

--- 3rdYearProject/BLL/QueryExceptionHandling.cs
+++ 3rdYearProject/BLL/QueryExceptionHandling.cs
@@ -8,7 +8,24 @@ namespace BLL
 {
     class QueryExceptionHandling
     {
-        
+        //Main form
+        //tussen switch query en combo gives you an option if you want to switch 
+        //- no selected show the previous options
+
+        //Data type checking all 
+        /* tab - values, where, Set, Having, Procedures.
+         * table creation when column selected check if the data types are the same.
+         */
+
+        //exception catcher but must not stop program.
+
+        //Color selection from menu strip.
+
+        //check if query will run or not run (using SQL built in function)
+
+        // main menu, on tabs. make sure that the right datatype is filled in
+
+
     }
 
     
